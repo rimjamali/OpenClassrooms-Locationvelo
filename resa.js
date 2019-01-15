56c6ed8f8e2d7249f862f545d7b6b740acf44f6b
Rim Jamali <45394657+rimjamali@users.noreply.github.com>
2019-01-15 22:29:15 +0100

    Update resa.js

--- resa.js
+++ resa.js
@@ -1,21 +1,49 @@
-
-
-//RESERVATION.JS 
-
 var Reservation = {
-    prenom: '',
-    nom: '',
-    signature: '',
+
+    // Propriétés
+    identite: {
+        prenom: '',
+        nom: '',
+    },
+    signature: null,
     station: null,
     date: null,
 
-    remplirIdentite: function(prenom, nom) {
-        this.prenom = prenom
-        this.nom = nom
+    // Méthodes
+    init: function() {
+        var self = this;
+        $('#bookingBtn').click(function() {
+            self.identite.prenom = $('#prenom').val();
+            self.identite.nom = $('#nom').val();
+
+            // Sauvegarder l'identité et la station dans le local storage
+            if ((self.identite.prenom === '') || (self.identite.nom === '')) {
+                $('#message-erreur').show(); 
+                $('#canvas-click').hide();
+            } else {
+                $('#message-erreur').hide(); 
+                $('#message-signature').show();
+                $('#canvas-click').show();
+                localStorage.setItem('identite', JSON.stringify(self.identite));
+            }
+
+        })
+    },
+    
+    completerStation: function(station) {
+        this.station = station;
+        sessionStorage.setItem('station', JSON.stringify(station))
     }
 
-    $('.btn').click(function() {
-    alert( "Handler for .click() called." );
-});
+/*
+    canvasSignature: function(signature) {
+        this.signature = signature;
+        var self = this;
+        $('')
 
+    }
+
+ */   
 }
+
+
