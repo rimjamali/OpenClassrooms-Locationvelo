1fb344865aca355943178220e30137631dbc695a
Rim Jamali <45394657+rimjamali@users.noreply.github.com>
2019-01-15 22:42:41 +0100

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -3,17 +3,14 @@ var Countdown = {
 	
 	
 	//propriétés
-	delay : 1200000, // 20min * 60sec * 1000ms
 	minutes: null,
     secondes: null,
     timer: null,
 
 
 	// Méthodes
-    
     start: function() {
-        
-        this.timer = setInterval(this.decompte, 1000);
+        this.timer = setInterval(this.decompte.bind(this), 1000);
     },
 
     clearTimer: function() {
@@ -35,24 +32,19 @@ var Countdown = {
         //var secondesRestantes = 59 - secondesEcoulees
         //var minutesRestantes = 19 - minutesEcoulees
         
-        var secondesRestantes = 05 - secondesEcoulees
+        var secondesRestantes = 35 - secondesEcoulees
         var minutesRestantes = 00 - minutesEcoulees
 
         $('#minutes').html(minutesRestantes)
         $('#secondes').html(secondesRestantes)
 
         if (secondesRestantes === 0 && minutesRestantes === 0) {
-            self.clearTimer; //ne fonctionne pas, le timer se met à -25000000
+            self.clearTimer(); 
             sessionStorage.clear();
-             $('.message-reservation').removeClass('message-reservation'); //ne //fonctionne pas
+            $('#messageResa').addClass('message-reservation');
             $('.message-fin-reservation').toggleClass('message-fin-reservation');
             
-        }; // else if (on rafraichit la page) {
-        //    self.start;
-    //    } else if (on ferme la page) {
-      //      self.clearTimer;
-        //    annuler résa
-       // }
+        }; 
 		
 	} 
 }
