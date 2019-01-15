d3df72712e26d1c079eda771402f281fc03ddbfb
Rim Jamali <45394657+rimjamali@users.noreply.github.com>
2019-01-15 21:27:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-$(function() {
+ $(function() {
     
     var diapoObj = Object.create(Diaporama);
     diapoObj.initialiser();
     
    var mapsObj = Object.create(Maps);
    mapsObj.init();
-    
 
 
-})
+  //  var reservationObj = Object.create(Reservation);
+
+   // var stationObj = Object.create(Station)
+   // stationObj.recupererStations(mapsObj, reservationObj);
+
+});
+
+
+
 
 
