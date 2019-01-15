d54458559ac151f56ebaf8af2dbd26629df1b2b6
Rim Jamali <45394657+rimjamali@users.noreply.github.com>
2019-01-15 22:23:21 +0100

    Update station.js

--- station.js
+++ station.js
@@ -1,56 +1,46 @@
 
 var Station = {
-
+    
+    // Propriétés
     stations: [],
-    reservationObj: null,
-    mapsObj: null,
 
-    recupererStations: function(mapsObj, reservationObj) {
+
+    // Méthodes
+    init: function(mapObj, resaObj) {
         var self = this;
 
-        self.mapsObj = mapsObj
-        self.reservationObj = reservationObj
-
-        $.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=453628a242d0db7b1d2972afa3421f5460dddc0d")
+        $.getJSON('https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=453628a242d0db7b1d2972afa3421f5460dddc0d')
             .done(function(data) {
                 console.log('reponse jcdecaux: ', data);
                 self.stations = data;
 
+                // Afficher les stations sur la carte
                 var markers = [];
-                self.stations.forEach(function(station) {
-                    var marker = self.mapsObj.addMarker(self.map, station.position.lat, station.position.lng)
+                self.stations.forEach(function(station, stationIx, stations) {
+                    var marker = mapObj.addMarker(station.position.lat, station.position.lng);
 
-                    self.mapsObj.addEvent(marker, 'click', function() {
-
+                    mapObj.addEvent(marker, 'click', function() {
+                        
                         $('.label-ligne').show();
                         $('.adresse-station').html(station.address);
                         station.status = (station.status === 'OPEN') ? ' Ouverte' : ' Fermée';
-                        $('.etat-station').html(station.status);
+                        $('.etat-station').html( station.status);
                         $('.velos-disponibles').html(station.available_bikes);
                         $('.emplacements-libres').html(station.available_bike_stands);
-
-                        if ((station.available_bikes === 0) || (station.status === 'CLOSED')) {
-                            $('.form-et-bouton').hide();
+                        if ((station.available_bikes === 0) || (station.status === ' Fermée')) {
+                            $('.form-et-bouton').hide(); 
                         } else {
                             $('.form-et-bouton').show();
                         }
-                    });
 
-                    $('#btnReserver').click(self.reservationClick.bind(self));
-                    
+                        resaObj.completerStation(station);
+                    })
                     markers.push(marker);
-                });
-
-                self.mapsObj.cluster(markers, self.map)
+                })
+                mapObj.addMarkerClusterer(markers);
             })
             .fail(function(err) {
-                console.log('erreur: ', err)
-            });
-    },
-    reservationClick: function() {
-        var prenom = $('#prenom').val()
-        var nom = $('#nom').val()
-        self.reservationObj.remplirIdentite(prenom, nom)
+                console.log('Les erreurs de JC Decaux sont les suivantes : ', err);
+            })
     }
 }
-
