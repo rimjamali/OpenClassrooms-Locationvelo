db50f27c46ea7ae76669dea0b93f4afa115b306e
Rim Jamali <45394657+rimjamali@users.noreply.github.com>
2019-01-15 21:19:25 +0100

    Create resa.js

---
+++ resa.js
@@ -0,0 +1,18 @@
+var Reservation = {
+    prenom: '',
+    nom: '',
+    signature: '',
+    station: null,
+    date: null,
+
+    remplirIdentite: function(prenom, nom) {
+        this.prenom = prenom
+        this.nom = nom
+    }
+
+    $('.btn').click(function() {
+    alert( "Handler for .click() called." );
+});
+
+}
+
