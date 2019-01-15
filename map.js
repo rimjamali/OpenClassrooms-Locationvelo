96549a73d8e1b77b1cef1b8f4ae8761988f1f1d5
Rim Jamali <45394657+rimjamali@users.noreply.github.com>
2019-01-15 22:12:25 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,14 +1,14 @@
-var Maps = {
+var Carte = {
 
     // Propriétés
-    map: null,
+    carte: null,
     defaultZoom: 13,
     defaultCenter: {lat: 47.218371, lng: -1.553621},
 
     // Méthodes
     init: function() {
 
-        this.map = new google.maps.Map($('#map')[0], {
+        this.carte = new google.maps.Map($('#map')[0], {
             zoom: this.defaultZoom,
             center: this.defaultCenter,
         });
@@ -17,7 +17,7 @@ var Maps = {
 
     addMarker: function(lat, lng) {
         var marker = new google.maps.Marker({
-            map: this.map,
+            map: this.carte,
             position: {lat: lat, lng: lng},
         });
         return marker;
@@ -25,7 +25,7 @@ var Maps = {
 
     addMarkerClusterer: function(markers) {
         var markerClusterer = new MarkerClusterer(
-            this.map, 
+            this.carte, 
             markers, 
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'}
         );
